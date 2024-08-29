da4fa8d9ac1836536d63abda03cc37eda9b278a9
Krishr21 <113686462+Krishr21@users.noreply.github.com>
2024-08-30 02:35:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-let todo = [];
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
 
-let req =prompt("please enter your request");
+btn.addEventListener("click",function (){
+    let item = document.createElement("li");
+    item.innerText=inp.value;
 
-console.log(req);
+    let delBtn = document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+    item.appendChild(delBtn);
 
-while(true){
-    if(req == "quit"){
-        console.log("quiting app");
-        break;
+    ul.appendChild(item);
+    inp.value = "";
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem = event.target.parentElement;
+        listItem.remove();
+        console.log("DELETED");
     }
+});
 
-    if(req == "list"){
-        console.log("---------");
-        for(let i=0; i<todo.length;i++){
-            console.log(todo[i]);
-        }
-        console.log("---------");
-    }
-    else if(req == "add"){
-        let task = prompt("please enter the task you want to add");
-        todo.push(task);
-        console.log("task added");
-    }
-    else if(req == "delete"){
-        let idx = prompt("please enter the task index");
-        todo.splice(idx,1);
-        console.log("task deleted");
-    } else{
-        console.log("wrong request");
-    }
+// let delBtns = document.querySelectorAll(".delete");
+//     for (delBtn of delBtns){
+//         delBtn.addEventListener("click", function(){
+//             let par = this.parentElement;
+//             par.remove();
+//         });
+//     }
 
-    req =prompt("please enter your request");
-}
